6267ebc1807c6819d9df7235fe9d746a03b5e495
darlingD <2442105425@qq.com>
2018-11-24 10:11:03 +0800

    dengluzhuce

--- MusicApp/src/components/commons/Aside.vue
+++ MusicApp/src/components/commons/Aside.vue
@@ -5,7 +5,10 @@
 				<div class="con" v-show="show">
 					<div class="myInfo">
 						<i class="fa fa-user-circle-o"></i>
-						<span>1885464546</span>
+						<span></span>
+						<router-link to='/reg' class="zhuce">注册</router-link>
+			
+						<router-link to='/login' class="denglu">登录</router-link>
 					</div>
 					<ul>
 						<li  @click='goPage(item.path)' 
@@ -38,10 +41,10 @@
 			return {
 				navList:[
 						{title:'首页',path:'/home',name:'Home'},
-						{title:'推荐',path:'/recommend',name:'Recommend'},
-						{title:'歌手',path:'/singer',name:'Singer'},
-						{title:'榜单',path:'/leaderboard',name:'Leaderboard'},
-						{title:'我的',path:'/mine',name:'Mine'}]
+						{title:'我的收藏',path:'/recommend',name:'Recommend'},
+						{title:'喜欢',path:'/singer',name:'Singer'},
+						{title:'我的',path:'/leaderboard',name:'Leaderboard'},
+						{title:'设置',path:'/mine',name:'Mine'}]
 						
 			}
 		},
@@ -69,6 +72,7 @@
 				background: #fff;
 				z-index:9999;
 				.myInfo{
+					display:flex;
 				.w(265);
 				.h(50);
 				color:#000;
@@ -76,12 +80,31 @@
 				.pd(0,0,0,20);
 				i{
 					.fs(30);
+					.mg(10,0,0,0);
+					color:blue;
 				}
 				span{
 					.pd(0,0,0,20);
 				}
+				.zhuce{
+				.h(34);
+				.w(73);
+				text-align:center;
+				.lh(34);
+				background:#EEE;
+				.mg(10,10,0,20);
+				}
+				.denglu{
+					.h(34);
+					.w(73);
+					text-align:center;
+					.lh(34);
+					background:#EEE;
+					.mg(10,20,0,10);
+				}
 			}
 			ul{
+				.pd(20,0,0,0);
 		 		li{
 		 			display: flex;
 		 			justify-content: space-between;
---
+++ MusicApp/src/components/pages/Login/Login.vue
@@ -0,0 +1,73 @@
+<template>
+	<div class="login">
+		<div class="contain">
+			<div class="dl">
+				<i> 登录</i>
+			</div>
+		
+		<input type="text" name=""  value=""  placeholder="手机/邮箱"/><br />
+		<input type="text" name=""  value=""  placeholder="密码"/>
+		</div>
+		<div class="forget">
+			<span>忘记密码</span>
+			<span>用户注册</span>
+		</div>
+		<input type="button" name="" id="" value="登录"  class="dianji"/>
+	</div>
+</template>
+
+<script>
+	export default{
+	name:'BottomBtn2',
+	components:{},
+//	props:'',
+	data(){
+		return {
+			disc:"登录"
+		}
+
+	}
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../../styles/main.less';
+.login{
+	/*background:red;*/
+	.mg(100,0,0,0);
+	.dl{
+		text-align: center;
+		.lh(60);
+		.fs(16);
+		color:#999;
+		font-family: "Arial","Microsoft YaHei","黑体","宋体",sans-serif;
+
+		
+	}
+	input{
+		.w(330);
+		.h(44);
+		border:1px solid #666;
+		.mg(10,0,10,20);
+		
+	}
+	.forget{
+		.w(320);
+		.mg(40,0,10,30);
+		display: flex;
+		justify-content: space-between;
+		color: cornflowerblue;
+		.fs(12);
+	}
+
+	.dianji{
+		.w(150);
+		.h(38);
+		.mg(40,0,0,110);
+		border-radius:5%;
+		background: cornflowerblue;
+		color:#fff;
+		.fs(14);
+	}
+}
+</style>
